92a80d29ea305a6fa499ecfa2442fe9515173659
yg1523 <149255036+yg1523@users.noreply.github.com>
2025-07-16 13:34:19 +0100

    update main code

--- Untitled-2.jl
+++ Untitled-2.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using ClassicalOrthogonalPolynomials, SingularIntegrals, QuadGK
+using ContinuumArrays
 P = Legendre()
 
 n = 5
@@ -9,3 +10,9 @@ stieltjes(P, 0.1+0im)[1:n]/(-2π*im)
 x = ClassicalOrthogonalPolynomials.grid(P, n)
 C⁺ = stieltjes(P, x .+ 0im)[:,1:n]/(-2π*im) 
 C⁻ = stieltjes(P, x .- 0im)[:,1:n]/(-2π*im)
+
+using Test
+# evaluate the stiletjes transform of exp(x)
+c = transform(P, exp) # Legendre coefficients
+f = P*c # Legendre expansion
+@test f[0.1] ≈ exp(0.1)
