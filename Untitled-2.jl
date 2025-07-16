732bb36b19b2ff7dd579f97ddba344e9b3c8e0e2
yg1523 <149255036+yg1523@users.noreply.github.com>
2025-07-16 13:23:22 +0100

    update main code

--- Untitled-2.jl
+++ Untitled-2.jl
@@ -5,3 +5,7 @@ P = Legendre()
 n = 5
 #normalize to use Cauchy Operator
 stieltjes(P, 0.1+0im)[1:n]/(-2π*im)
+
+x = ClassicalOrthogonalPolynomials.grid(P, n)
+C⁺ = stieltjes(P, x .+ 0im)[:,1:n]/(-2π*im) 
+C⁻ = stieltjes(P, x .- 0im)[:,1:n]/(-2π*im)
