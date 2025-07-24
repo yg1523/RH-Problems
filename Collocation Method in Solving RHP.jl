2ab7e1f851483b685fd1f04d582138eb5ea3b13b
yg1523 <149255036+yg1523@users.noreply.github.com>
2025-07-24 04:59:23 +0100

    update main code

--- Collocation Method in Solving RHP.jl
+++ Collocation Method in Solving RHP.jl
@@ -28,3 +28,32 @@ u_n(s) = sum(c[k] * P[s, k] for k in 1:n)
 #verify that this is a good practice of collocation method
 residual = [u_n(x[j]) - (G(x[j]) - 1 + (C⁻ * c)[j] * (G(x[j]) - 1)) for j in 1:n]
 println("maximum residual: ", maximum(abs.(residual)))
+
+
+using QuadGK
+
+function Phi(z, c, P)
+    
+    integrand(t) = u_n(t) / (t - z)
+    # calculate the Cauchy Transform of our approximated funciton
+    C_u, error_estimate = quadgk(integrand, -1, 1; rtol=1e-8)
+    return 1 + C_u / (2π * im)
+end
+
+s = 0.7
+Φ⁺ = Phi(s + 1e-10im, c, P)
+Φ⁻ = Phi(s - 1e-10im, c, P)
+jump_condition = Φ⁺ ≈ Φ⁻ * G(s)
+
+
+
+function Phi_stieltjes(z, c, P)
+    S = stieltjes(P, z)[1:length(c)]/(-2π*im)
+    1 + dot(c, S)
+end
+
+Φ⁺ = Phi_stieltjes(s + 0im, c, P)
+Φ⁻ = Phi_stieltjes(s - 0im, c, P)
+jump_condition = Φ⁺ ≈ Φ⁻ * G(s)
+
+
